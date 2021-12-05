b72df56b22e373369fe1875875167c6e3c22619a
Wouter De Troyer <289099+wouterdt@users.noreply.github.com>
2021-12-05 08:57:06 +0100

    add tag and taggroup
    add tag and taggroup

---
+++ src/abook/tag.php
@@ -0,0 +1,20 @@
+<?php
+use Joomla\CMS\Factory;
+// No direct access.
+defined('_JEXEC') or die();
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_abook/tables');
+
+class AbookApiResourceTag extends ApiResource
+{ 
+    public function put(){
+        $table =  JTable::getInstance("Tag", "AbookTable", array());
+        //no idea how to make joomla do this
+        $data = json_decode(file_get_contents("php://input"),true);
+        $json = $data['data'];
+        if ($json['id'] != 0){
+            $table->load($json['id']);
+        }
+        $table->save($json);
+        $this->plugin->setResponse($table->getPrimaryKey());
+    }
+}
